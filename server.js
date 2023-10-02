dd0db67230be29c56d332d7939575455efac30b9
Demirev Nikita <63374748+NickDemiman@users.noreply.github.com>
2023-10-02 09:51:01 +0300

    server on express

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
-const http = require("http");
-const fs = require('fs');
+const express = require('express');
+const path = require('path');
 
 const port = 3000
+const publicDir = 'public'
 
-http.createServer(function(request, response){
-      
-    console.log(`Запрошенный адрес: ${request.url}`);
-    // получаем путь после слеша
-    const filePath = 'public/' + request.url.substr(1);
-    // смотрим, есть ли такой файл
-    fs.access(filePath, fs.constants.R_OK, err => {
-        // если произошла ошибка - отправляем статусный код 404
-        if(err){
-            response.statusCode = 404;
-            response.end("Resourse not found!");
-        }
-        else{
-            fs.createReadStream(filePath).pipe(response);
-        }
-      });
-}).listen(port, function(){
-    console.log("Server started at " + port);
+const app = express();
+
+app.use(express.static(path.join(__dirname, publicDir)))
+
+app.get('/', (req, res) => {
+    res.send({ message: 'Hello WWW!' });
+});
+
+app.listen(port, () => {
+    console.log('Application listening on port ', port, '!');
 });
