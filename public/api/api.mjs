a2dc1ba172a01783f11c9174a5f216d3311b16dc
Demirev Nikita <nikdem00@gmail.com>
2023-10-06 18:22:46 +0300

    Update api.mjs

--- public/api/api.mjs
+++ public/api/api.mjs
@@ -1,5 +1,5 @@
 const API = {
-    ADRESS_BACKEND: 'http://localhost:8080/api/v1/',
+    ADRESS_BACKEND: 'http://84.23.53.28:8080/api/v1/',
     SIGNUP: 'signup',
     SIGNIN: 'signin',
     GET_POST_LIST: 'post/get_list'
