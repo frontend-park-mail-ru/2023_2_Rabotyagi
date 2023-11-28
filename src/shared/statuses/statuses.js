ecb7f82838d1631f3fb7cafb0f03aae99fd45b38
unknown <tanyaemelyanova2015@gmail.com>
2023-11-28 22:15:45 +0300

    update statuses format

--- src/shared/statuses/statuses.js
+++ src/shared/statuses/statuses.js
@@ -1,81 +1,81 @@
-/**
- * STATUS_RESPONSE_SUCCESSFUL - используется когда ответ успешный
- * @constant
- * @type {number}
- */
-export const STATUS_RESPONSE_SUCCESSFUL = 2000;
+class Statuses {
+    /**
+     * STATUS_RESPONSE_SUCCESSFUL - используется когда ответ успешный
+     * @type {number}
+     */
+    STATUS_RESPONSE_SUCCESSFUL = 2000;
 
-/**
- * @summary определяет является ли запрос успешным
- * @param {Object} body body ответа с status полем
- * @return {boolean}
- */
-export function IsSuccessfulRequest(body) {
-    return body?.status === STATUS_RESPONSE_SUCCESSFUL;
-}
+    /**
+     * STATUS_REDIRECT_AFTER_SUCCESSFUL - используется когда ответ успешный и нужно перенаправить на другой ресурс
+     * @type {number}
+     */
+    STATUS_REDIRECT_AFTER_SUCCESSFUL = 3003;
 
-/**
- * STATUS_REDIRECT_AFTER_SUCCESSFUL - используется когда ответ успешный и нужно перенаправить на другой ресурс
- * @constant
- * @type {number}
- */
-export const STATUS_REDIRECT_AFTER_SUCCESSFUL = 3003;
+    /**
+     * STATUS_BAD_FORMAT_REQUEST - используется когда был отправлен неккоректный запрос на сервер.
+     * Содержимое этой ошибки нужно только для разработчиков. Не показывать пользователям!
+     * @type {number}
+     */
+    STATUS_BAD_FORMAT_REQUEST = 4000;
 
-/**
- * @summary определяет является ли ответ rediret-ом
- * @param {Object} body body ответа с status полем
- * @return {boolean}
- */
-export function IsRedirectResponse(body) {
-    return body?.status === STATUS_REDIRECT_AFTER_SUCCESSFUL;
-}
+    /**
+     * STATUS_BAD_CONTENT_REQUEST - используется когда был отправлен неккоректный запрос на сервер, но проблема в данных,
+     * введенных клиентом. Эти ошибки нужно показывать пользователям.
+     * @type {number}
+     */
+    STATUS_BAD_CONTENT_REQUEST = 4400;
 
-/**
- * STATUS_BAD_FORMAT_REQUEST - используется когда был отправлен неккоректный запрос на сервер.
- * Содержимое этой ошибки нужно только для разработчиков. Не показывать пользователям!
- * @constant
- * @type {number}
- */
-export const STATUS_BAD_FORMAT_REQUEST = 4000;
+    /**
+     * status_internal_server - используется когда произошла ошибка на сервере.
+     * @constant
+     * @type {number}
+     */
+    STATUS_INTERNAL_SERVER = 5000;
 
-/**
- * @summary определяет произошла ли ошибка из-за неккоретного формата запроса
- * @param {Object} body body ответа с status полем
- * @return {boolean}
- */
-export function IsBadFormatRequest(body) {
-    return body?.status === STATUS_BAD_FORMAT_REQUEST;
-}
+    /**
+     * @summary определяет является ли запрос успешным
+     * @param {Object} body body ответа с status полем
+     * @return {boolean}
+     */
+    IsSuccessfulRequest(body) {
+        return body?.status === this.STATUS_RESPONSE_SUCCESSFUL;
+    }
 
-/**
- * STATUS_BAD_CONTENT_REQUEST - используется когда был отправлен неккоректный запрос на сервер, но проблема в данных,
- * введенных клиентом. Эти ошибки нужно показывать пользователям.
- * @constant
- * @type {number}
- */
-export const STATUS_BAD_CONTENT_REQUEST = 4400;
+    /**
+     * @summary определяет является ли ответ rediret-ом
+     * @param {Object} body body ответа с status полем
+     * @return {boolean}
+     */
+    IsRedirectResponse(body) {
+        return body?.status === this.STATUS_REDIRECT_AFTER_SUCCESSFUL;
+    }
 
-/**
- * @summary определяет является ли ошибка такой, которую можно показать пользователю
- * @param {Object} body body ответа с status полем
- * @return {boolean}
- */
-export function IsUserError(body) {
-    return body?.status === STATUS_BAD_CONTENT_REQUEST;
-}
+    /**
+     * @summary определяет произошла ли ошибка из-за неккоретного формата запроса
+     * @param {Object} body body ответа с status полем
+     * @return {boolean}
+     */
+    IsBadFormatRequest(body) {
+        return body?.status === this.STATUS_BAD_FORMAT_REQUEST;
+    }
 
-/**
- * status_internal_server - используется когда произошла ошибка на сервере.
- * @constant
- * @type {number}
- */
-export const STATUS_INTERNAL_SERVER = 5000;
+    /**
+     * @summary определяет является ли ошибка такой, которую можно показать пользователю
+     * @param {Object} body body ответа с status полем
+     * @return {boolean}
+     */
+    IsUserError(body) {
+        return body?.status === STATUS_BAD_CONTENT_REQUEST;
+    }
 
-/**
- * @summary определяет произошла ли ошибка на сервере
- * @param {Object} body body ответа с status полем
- * @return {boolean}
- */
-export function IsInternalServerError(body) {
-    return body?.status === STATUS_INTERNAL_SERVER;
-}
+    /**
+     * @summary определяет произошла ли ошибка на сервере
+     * @param {Object} body body ответа с status полем
+     * @return {boolean}
+     */
+    IsInternalServerError(body) {
+        return body?.status === STATUS_INTERNAL_SERVER;
+    }
+};
+
+export default new Statuses();
