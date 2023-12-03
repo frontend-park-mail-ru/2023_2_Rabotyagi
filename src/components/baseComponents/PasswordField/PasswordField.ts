c307441d0e333c8c08a6692ddac853ceb53940c2
Demirev Nikita <nikdem00@gmail.com>
2023-12-03 23:25:33 +0300

    Update PasswordField.ts

--- src/components/baseComponents/PasswordField/PasswordField.ts
+++ src/components/baseComponents/PasswordField/PasswordField.ts
@@ -4,7 +4,7 @@ import { Component } from '../snail/component';
 import { createComponent, createElement } from '../snail/vdom/VirtualDOM';
 
 import { Button } from '../button/Button';
-import { PasswordInputProps, BaseInput, BaseInputProps } from '../input/Input';
+import { PasswordInputProps, BaseInput } from '../input/Input';
 
 type FieldType = 'password' | 'text';
 
@@ -23,13 +23,6 @@ export class PasswordField extends Component<PasswordInputProps, PasswordFieldSt
     setType(newType: FieldType) {
         // к сожалению, пришлось вставить сюда костыль до окончательного решения
         if (newType !== this.state.type) {
-            // this.setState((state) => {
-            //     state = { ...this.state };
-            //     state.preventType = this.state.type;
-            //     state.type = newType;
-
-            //     return state;
-            // });
             this.setState({
                 preventType: this.state.type,
                 type: newType,
